0c6b98dbf8cf58613cde270b807fa369a72c1dff
RKK <masterkrish2001@gmail.com>
2020-09-11 18:43:09 +0530

    deleted an unnecessary variable

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,15 +5,3 @@ const apiKey = 'fcff0872';
 
 const URL = 'http://www.omdbapi.com/';
 
-InputDecoration kTextFieldInputDecoration = InputDecoration(
-  filled: true,
-  fillColor: Colors.white,
-  hintText: 'Enter Movie/Series Name',
-  hintStyle: GoogleFonts.philosopher(color: Colors.grey),
-  border: OutlineInputBorder(
-    borderRadius: BorderRadius.all(
-      Radius.circular(10),
-    ),
-    borderSide: BorderSide.none,
-  ),
-);
